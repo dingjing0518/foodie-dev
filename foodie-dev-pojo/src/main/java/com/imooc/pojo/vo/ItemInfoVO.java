c00f89469e390745e561eaba5fa18fd5a579d408
dingjing_ext <dingjing@wx.bigcloudsys.com>
2020-06-12 00:18:19 +0800

    商品详情和商品推荐

---
+++ foodie-dev-pojo/src/main/java/com/imooc/pojo/vo/ItemInfoVO.java
@@ -0,0 +1,50 @@
+package com.imooc.pojo.vo;
+
+import com.imooc.pojo.Items;
+import com.imooc.pojo.ItemsImg;
+import com.imooc.pojo.ItemsParam;
+import com.imooc.pojo.ItemsSpec;
+
+import java.util.List;
+
+/**
+ * 商品详情VO
+ */
+public class ItemInfoVO {
+    private Items item;
+    private List<ItemsImg> itemImgList;
+    private List<ItemsSpec> itemSpecList;
+    private ItemsParam itemParam;
+
+    public Items getItem() {
+        return item;
+    }
+
+    public void setItem(Items item) {
+        this.item = item;
+    }
+
+    public List<ItemsImg> getItemImgList() {
+        return itemImgList;
+    }
+
+    public void setItemImgList(List<ItemsImg> itemImgList) {
+        this.itemImgList = itemImgList;
+    }
+
+    public List<ItemsSpec> getItemSpecList() {
+        return itemSpecList;
+    }
+
+    public void setItemSpecList(List<ItemsSpec> itemSpecList) {
+        this.itemSpecList = itemSpecList;
+    }
+
+    public ItemsParam getItemParam() {
+        return itemParam;
+    }
+
+    public void setItemParam(ItemsParam itemParam) {
+        this.itemParam = itemParam;
+    }
+}
---
+++ foodie-dev-pojo/src/main/java/com/imooc/pojo/vo/SimpleItemVO.java
@@ -0,0 +1,34 @@
+package com.imooc.pojo.vo;
+
+/**
+ *最新6个商品VO
+ */
+public class SimpleItemVO {
+    private Integer itemId;
+    private String itemName;
+    private String itemUrl;
+
+    public Integer getItemId() {
+        return itemId;
+    }
+
+    public void setItemId(Integer itemId) {
+        this.itemId = itemId;
+    }
+
+    public String getItemName() {
+        return itemName;
+    }
+
+    public void setItemName(String itemName) {
+        this.itemName = itemName;
+    }
+
+    public String getItemUrl() {
+        return itemUrl;
+    }
+
+    public void setItemUrl(String itemUrl) {
+        this.itemUrl = itemUrl;
+    }
+}
